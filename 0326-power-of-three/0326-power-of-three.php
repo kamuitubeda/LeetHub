7ac6e7f24fd54cc7e191caed65ce7f8c3c957c2f
Saad Ahyat Hasan <kamuitubeda@gmail.com>
2023-06-06 13:16:20 +0700

    Time: 70 ms (94.44%), Space: 19.3 MB (22.22%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.php
@@ -0,0 +1,19 @@
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return Boolean
+     */
+    function isPowerOfThree($n) {
+        if($n <= 0) return false;
+        
+        while($n > 1) {
+            if($n%3 != 0) {
+                return false;
+            }
+            $n /= 3;
+        }
+        
+        return true;
+    }
+}

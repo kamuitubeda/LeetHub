abdebaa15bfa4303a81c37539e4f688058eacd15
Saad Ahyat Hasan <kamuitubeda@gmail.com>
2023-06-07 12:09:44 +0700

    Time: 78 ms (98.41%), Space: 51 MB (89.33%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    const len = nums.length
+    k = (k % len)
+
+    let end = nums.splice(len - k)
+
+    nums.splice(0,0,...end)
+};
